b5dbbf84e710813b3cf60d3f7d058a4063e519c2
youyeon11 <yurry45@naver.com>
2025-12-02 15:04:39 +0900

    del: 불필요한 클래스 삭제

--- src/main/java/com/yachaerang/yachaerangbatch/configuration/step/WeeklyPriceStepConfig.java
+++
@@ -1,4 +0,0 @@
-package com.yachaerang.yachaerangbatch.configuration.step;
-
-public class WeeklyPriceStepConfig {
-}
--- src/main/java/com/yachaerang/yachaerangbatch/domain/product/reader/ProductReader.java
+++
@@ -1,4 +0,0 @@
-package com.yachaerang.yachaerangbatch.domain.product.reader;
-
-public class ProductReader {
-}
