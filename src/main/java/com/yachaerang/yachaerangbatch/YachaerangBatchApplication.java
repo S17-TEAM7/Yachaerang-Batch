82c0075974fb6bfcf512244cd788585e1b2bde15
youyeon11 <yurry45@naver.com>
2025-12-02 15:05:56 +0900

    fix: Package 지정

--- src/main/java/com/yachaerang/yachaerangbatch/YachaerangBatchApplication.java
+++ src/main/java/com/yachaerang/yachaerangbatch/YachaerangBatchApplication.java
@@ -3,7 +3,7 @@ package com.yachaerang.yachaerangbatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yachaerang.yachaerangbatch")
 public class YachaerangBatchApplication {
 
     public static void main(String[] args) {
