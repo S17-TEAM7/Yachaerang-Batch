d4fd3a096db5d8549f6e6b1721e361fa88c1a283
youyeon11 <yurry45@naver.com>
2025-12-15 00:35:35 +0900

    feat: 월간 데이터 저장을 위한 Job Configuration 구현

--- src/main/java/com/yachaerang/yachaerangbatch/configuration/job/MonthlyPriceJobConfig.java
+++ src/main/java/com/yachaerang/yachaerangbatch/configuration/job/MonthlyPriceJobConfig.java
@@ -1,4 +1,123 @@
 package com.yachaerang.yachaerangbatch.configuration.job;
 
+import com.yachaerang.yachaerangbatch.domain.entity.MonthlyPrice;
+import com.yachaerang.yachaerangbatch.listener.JobCompletionListener;
+import com.yachaerang.yachaerangbatch.listener.StepExecutionListener;
+import com.yachaerang.yachaerangbatch.service.MonthlyPriceAggregationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
 public class MonthlyPriceJobConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final JobCompletionListener jobCompletionListener;
+    private final StepExecutionListener stepExecutionListener;
+
+    private final MonthlyPriceAggregationService monthlyPriceAggregationService;
+
+
+    private static final int CHUNK_SIZE= 100;
+
+    @Bean
+    public Job monthlyPriceJob(Step monthlyPriceStep) {
+        return new JobBuilder("monthlyPriceJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(jobCompletionListener)
+                .start(monthlyPriceStep)
+                .build();
+    }
+
+    /*
+    MonthlyPrice Step
+     */
+    @Bean
+    public Step monthlyPriceStep(
+            ListItemReader<MonthlyPrice> monthlyPriceReader,
+            ItemProcessor<MonthlyPrice, MonthlyPrice> monthlyPriceProcessor,
+            ItemWriter<MonthlyPrice> monthlyPriceWriter
+    ) {
+        return new StepBuilder("monthlyPriceStep", jobRepository)
+                .<MonthlyPrice, MonthlyPrice>chunk(CHUNK_SIZE, platformTransactionManager)
+                .listener(stepExecutionListener)
+                .reader(monthlyPriceReader)
+                .processor(monthlyPriceProcessor)
+                .writer(monthlyPriceWriter)
+                .faultTolerant()
+                .retryLimit(3)
+                .retry(Exception.class)
+                .build();
+    }
+
+    /**
+     * Reader 스텝
+     * @param year : 대상 년도
+     * @param month : 대상 월
+     * @return
+     */
+    @Bean
+    @StepScope
+    public ListItemReader<MonthlyPrice> monthlyPriceReader(
+            @Value("#{jobParameters['year']}") String year,
+            @Value("#{jobParameters['month']}") String month
+    ) {
+        int y = Integer.parseInt(year);
+        int m = Integer.parseInt(month);
+
+        List<MonthlyPrice> monthlyPriceList =
+                monthlyPriceAggregationService.getMonthlyAggregatedPrices(y, m);
+
+        log.info("ListItemReader 초기화 - 최종 리스트 크기: {}", monthlyPriceList.size());
+        return new ListItemReader<>(monthlyPriceList);
+    }
+
+    /**
+     * Processor Step
+     * @return
+     */
+    @Bean
+    public ItemProcessor<MonthlyPrice, MonthlyPrice> monthlyPriceProcessor() {
+        return item -> {
+            if (item.getPriceCount() == 0) {
+                log.debug("priceCount가 0이므로 스킵: {}", item.getProductCode());
+                return null;
+            }
+            return item;
+        };
+    }
+
+    /**
+     * Writer 스텝
+     * 집계 서비스를 통해 대신 MyBatis 실행
+     * @return
+     */
+    @Bean
+    public ItemWriter<MonthlyPrice> monthlyPriceWriter() {
+        return chunk -> {
+            List<MonthlyPrice> items = new ArrayList<>(chunk.getItems());
+            log.info("저장할 데이터 : {} 건", items.size());
+            monthlyPriceAggregationService.saveMonthlyPrices(items);
+        };
+    }
 }
