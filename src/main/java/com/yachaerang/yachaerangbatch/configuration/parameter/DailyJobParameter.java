04b505f94a8983de5587c2d0adf85bc643906c98
youyeon11 <yurry45@naver.com>
2025-12-15 08:45:12 +0900

    refactor: 패키지명 오타 수정(parameter)

--- src/main/java/com/yachaerang/yachaerangbatch/configuration/parameter/DailyJobParameter.java
+++ src/main/java/com/yachaerang/yachaerangbatch/configuration/parameter/DailyJobParameter.java
@@ -1,4 +1,4 @@
-package com.yachaerang.yachaerangbatch.configuration.parmameter;
+package com.yachaerang.yachaerangbatch.configuration.parameter;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
